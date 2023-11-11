ca1517a53e7968b301c29db9c2fc07a8d7b8b467
mattdoestech <mattdoesattractivetech@outlook.com>
2023-11-11 09:06:58 -0500

    fix circular import

--- atracktive/urls.py
+++ atracktive/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from catalogue import views as catalogueViews
 
 urlpatterns = [
-    path("", include(catalogueViews.home)),
+    path("", include("catalogue.urls")),
     path("admin/", admin.site.urls),
 ]
