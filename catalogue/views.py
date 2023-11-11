096e6cefd384a5c807f465e4faa6f69e56ce084f
mattdoestech <mattdoesattractivetech@outlook.com>
2023-11-11 14:02:10 -0500

    add html pages

--- catalogue/views.py
+++ catalogue/views.py
@@ -2,10 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('<h1>Welcome to Home Page</h1>')
+    return render(request, 'home.html')
 
 def about(request):
-    return HttpResponse('<h1>Welcome to About Page</h1>')
+    return render(request, 'about.html')
 
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the catalogue index.")
