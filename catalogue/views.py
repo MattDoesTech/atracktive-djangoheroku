9f1183c39c4fd337509f9ab407b82e122a0184c8
mattdoestech <mattdoesattractivetech@outlook.com>
2023-11-11 13:49:39 -0500

    adding about page

--- catalogue/views.py
+++ catalogue/views.py
@@ -4,5 +4,8 @@ from django.shortcuts import render
 def home(request):
     return HttpResponse('<h1>Welcome to Home Page</h1>')
 
+def about(request):
+    return HttpResponse('<h1>Welcome to About Page</h1>')
+
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the catalogue index.")
