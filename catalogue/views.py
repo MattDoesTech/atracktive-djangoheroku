deb61e73ab1d6423d019c3ceb55fe91278c30847
mattdoestech <mattdoesattractivetech@outlook.com>
2023-11-11 14:07:02 -0500

    pass data to html

--- catalogue/views.py
+++ catalogue/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'name': 'John Doe'})
 
 def about(request):
     return render(request, 'about.html')
