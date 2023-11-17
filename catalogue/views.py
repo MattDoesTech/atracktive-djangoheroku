4ec80a81ee736373ebaa459ca10ed45e29afc714
mattdoestech <mattdoesattractivetech@outlook.com>
2023-11-16 22:53:20 -0500

    get search return response

--- catalogue/views.py
+++ catalogue/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {'name': 'John Doe'})
+    searchTrack = request.GET.get('searchTrack')
+    return render(request, 'home.html', {'searchTrack': searchTrack})
 
 def about(request):
     return render(request, 'about.html')
