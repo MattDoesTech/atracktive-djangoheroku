eb6fc989720b53ad31d3e18de2393ea510629b80
mattdoestech <mattdoesattractivetech@outlook.com>
2023-11-18 15:13:57 -0500

    adding signup page

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
+    path('signup/', views.signup, name='signup'),
+    # path('admin/', admin.site.urls),
+
 ]
--- catalogue/views.py
+++ catalogue/views.py
@@ -5,6 +5,10 @@ def home(request):
     searchTrack = request.GET.get('searchTrack')
     return render(request, 'home.html', {'searchTrack': searchTrack})
 
+def signup(request):
+    email = request.Get.get('email')
+    return render(request, 'signup.html', {'email':email})
+
 def about(request):
     return render(request, 'about.html')
 
