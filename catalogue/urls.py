d18b77ace7beab6a6e905bd344d6baaa0238fb7c
mattdoestech <mattdoesattractivetech@outlook.com>
2023-11-20 07:57:24 -0500

    add db and admin site

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
-    path('signup/', views.signup, name='signup'),
+    path("signup/", views.signup, name="signup"),
+    path("admin/", admin.site.urls),
     # path('admin/', admin.site.urls),
 
 ]
