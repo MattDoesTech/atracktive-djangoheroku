e93ce290a44ac6d70fca84ee822ec2132b4d212e
mattdoestech <mattdoesattractivetech@outlook.com>
2023-11-22 00:09:12 -0500

    correcting model to import

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Catalogue
-admin.site.register(Catalogue)
+from .models import Tracks
+admin.site.register(Tracks)
 # Register your models here.
