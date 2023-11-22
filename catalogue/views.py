5b7dc25094f5ad778eb5638a56561b6c7e03811c
mattdoestech <mattdoesattractivetech@outlook.com>
2023-11-22 15:53:34 -0500

    fix import

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from models import Track
+from .models import Track
 
 def home(request):
     searchTerm = request.GET.get('searchTerm')
