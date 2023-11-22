bbc3cd0aaa7e7ea4643c6dc04194897c2fcb4837
mattdoestech <mattdoesattractivetech@outlook.com>
2023-11-22 00:20:04 -0500

    adding capability to add audio files and links

--- catalogue/models.py
+++ catalogue/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Track(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
+    audio_file = models.FileField(blank=True,null=True)
+    audio_link = models.CharField(max_length=200,blank=True,null=True)
     image = models.ImageField(upload_to='catalogue/images/')
     url = models.URLField(blank=True)
 
