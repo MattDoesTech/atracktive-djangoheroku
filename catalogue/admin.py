50a7088c6dfeed9fff73c1f28ec0bef788f2d003
mattdoestech <mattdoesattractivetech@outlook.com>
2023-11-22 00:02:26 -0500

    add catalogue config in administration

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import catalogue
+admin.site.register(catalogue)
 # Register your models here.
--- catalogue/urls.py
+++ catalogue/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path
 from django.contrib import admin
 from . import views
@@ -6,6 +8,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("signup/", views.signup, name="signup"),
-
-
 ]
+urlpatterns += static(settings.MEDIA_URL, 
+  document_root=settings.MEDIA_ROOT)
