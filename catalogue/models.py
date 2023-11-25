4788413cc40795cacd56b982a5eb15d2b7d4095e
mattdoestech <mattdoesattractivetech@outlook.com>
2023-11-25 17:09:41 -0500

    update audio link

--- catalogue/models.py
+++ catalogue/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Track(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
-    audio_file = models.FileField(blank=True,null=True)
+    audio_file = models.FileField(blank=True,null=True, upload_to='catalogue/audio/')
     audio_link = models.CharField(max_length=200,blank=True,null=True)
     image = models.ImageField(upload_to='catalogue/images/')
     url = models.URLField(blank=True)
