d12f1c4c938696e270df8960c6deb2ed56287e51
mattdoestech <mattdoesattractivetech@outlook.com>
2023-11-29 00:24:24 -0500

    add models to admin site

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Track
+from .models import Artist
+from .models import Album
 admin.site.register(Track)
+admin.site.register(Album)
+admin.site.register(Artist)
 # Register your models here.
