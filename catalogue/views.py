0910c5dd1578f6a995bfd9e0034a61f29e850075
mattdoestech <mattdoesattractivetech@outlook.com>
2023-11-30 05:06:51 -0500

    testing

--- catalogue/views.py
+++ catalogue/views.py
@@ -2,11 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Track
 from .models import Album
+from .models import Artist
 
 def home(request):
     # searchTerm = request.GET.get('searchTerm')
     albums = Album.objects.all()
-    return render(request, 'home.html', {'albums': albums})
+    artists = Artist.objects.all()
+    tracks = Track.objects.all()
+    return render(request, 'home.html', {'albums': albums}, {'artists': artists}, {'tracks':tracks})
     # return render(request, 'home.html', {'searchTerm': searchTerm, 'tracks': tracks})
 
 def signup(request):
