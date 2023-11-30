2fbb0ef6ba41c17a6048bee56433670dd295b85f
mattdoestech <mattdoesattractivetech@outlook.com>
2023-11-30 05:17:14 -0500

    undo test

--- catalogue/views.py
+++ catalogue/views.py
@@ -7,9 +7,7 @@ from .models import Artist
 def home(request):
     # searchTerm = request.GET.get('searchTerm')
     albums = Album.objects.all()
-    artists = Artist.objects.all()
-    tracks = Track.objects.all()
-    return render(request, 'home.html', {'albums': albums}, {'artists': artists}, {'tracks':tracks})
+    return render(request, 'home.html', {'albums': albums})
     # return render(request, 'home.html', {'searchTerm': searchTerm, 'tracks': tracks})
 
 def signup(request):
